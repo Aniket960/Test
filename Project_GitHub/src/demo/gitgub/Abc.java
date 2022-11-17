37c97805edd1a26d7f69f86c580cfdc82864008c
anike <anike@LAPTOP-4I11EMS7>
2022-11-17 15:32:57 +0530

    By Aniket1

--- Project_GitHub/src/demo/gitgub/Abc.java
+++ Project_GitHub/src/demo/gitgub/Abc.java
@@ -1,9 +1,7 @@
 package demo.gitgub;
 
 public class Abc {
-public static void main(String[] args) {
-	System.out.println("GitHub Demo project");
-}
+ 
 	public void m1()
 	{
 		System.out.println("M1 Method");
@@ -11,5 +9,8 @@ public static void main(String[] args) {
 	public void m2()
 	{
 		System.out.println("m2 method");
+	}
+	public static void main(String[] args) {
+		System.out.println("GitHub Demo project");
 	}
 }
